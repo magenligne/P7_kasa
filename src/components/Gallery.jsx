43faa8c03d3b0f829c9044139a23041366e80924
magenligne <magenligne@outlook.fr>
2022-11-18 15:44:57 +0100

    logique carroussel d'images page logement

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,24 +1,54 @@
 import React from "react";
+import { useState } from "react";
 
 // import data from "../data.json";
 
 function Gallery(props) {
-  const lengthTabImages = props.images.length;
-  let index = 0;
+  // const lengthTabImages = props.images.length;
+  const [currentIndex, setCurrentIndex] = useState(0); // console.log("props.images:", props.images);
+
+  const tabImages = props.images.map((eachimg, keyImg) => (
+    <img
+      src={eachimg}
+      alt={props.altImages}
+      key={keyImg}
+      className="gallery_image"
+    />
+  ));
+  console.log(tabImages);
+  console.log("objet1:", tabImages[1]);
+  console.log("Keyobjet1:", tabImages[1].key);
+
   return (
     <div className="gallery">
-      <div className="boutons">
-        <a href="#" className="prevNext">
-          <i class="fa-solid fa-chevron-left"></i>
-        </a>
-        <a href="#" className="prevNext">
-          <i class="fa-solid fa-chevron-right"></i>
-        </a>
+      <div
+        className="boutonNext"
+        onClick={() => {
+          if (currentIndex === tabImages.length - 1) {
+            return setCurrentIndex(0);
+          }
+          return setCurrentIndex(currentIndex + 1);
+        }}
+      >
+        <i class="fa-solid fa-chevron-right"></i>
       </div>
-      <img src={props.images[index]} alt="cover" className="gallery_image" />
-      {/* {props.images.map((eachimg) => (
-        <img src={eachimg} alt={props.altImages} className="gallery_image" />
-      ))} */}
+      <div
+        className="boutonPrev"
+        onClick={() => {
+          if (currentIndex === 0) {
+            return setCurrentIndex(tabImages.length - 1);
+          }
+          return setCurrentIndex(currentIndex - 1);
+        }}
+      >
+        <i class="fa-solid fa-chevron-left"></i>
+      </div>
+
+      <img
+        src={props.images[currentIndex]}
+        alt="cover"
+        className="gallery_image"
+      />
     </div>
   );
 }
