f6bd920c95ca906428c69f5b00237406793ed971
Afesoh Brain <brainafesoh@gmail.com>
2022-11-21 23:57:04 +0100

    Retouches de style pour la div déroulante

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -30,46 +30,27 @@ const Apropos = () => {
       <div className="ContainerDivApropos">
         <DivMasquable
           title="Fiabilité"
-          render={() => (
-            <p className="pLogement">
-              Les annonces postées sur Kasa garantissent une fiabilité totale.
-              Les photos sont conformes aux logements, et toutes les
-              informations sont régulièrement vérifiées par nos équipes.
-            </p>
-          )}
+          render={() =>
+            "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+          }
         />
         <DivMasquable
           title="Respect"
-          render={() => (
-            <p className="pLogement">
-              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-              comportement discriminatoire ou de perturbation du voisinage
-              entraînera une exclusion de notre plateforme.
-            </p>
-          )}
+          render={() =>
+            "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+          }
         />
         <DivMasquable
           title="Service"
-          render={() => (
-            <p className="pLogement">
-              Nos équipes se tiennent à votre disposition pour vous fournir une
-              expérience parfaite. N'hésitez pas à nous contacter si vous avez
-              la moindre question.
-            </p>
-          )}
+          render={() =>
+            "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+          }
         />
         <DivMasquable
           title="Sécurité"
-          render={() => (
-            <p className="pLogement">
-              La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
-              pour les voyageurs, chaque logement correspond aux critères de
-              sécurité établis par nos services. En laissant une note aussi bien
-              à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
-              que les standards sont bien respectés. Nous organisons également
-              des ateliers sur la sécurité domestique pour nos hôtes.
-            </p>
-          )}
+          render={() =>
+            "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+          }
         />
       </div>
     </div>
